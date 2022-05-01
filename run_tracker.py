d55d32b65405e5fdd11d588876d60ae24636a2a8
eduardodut <eduardo_dut@edu.unifor.br>
2022-05-01 17:09:43 -0300

    novo resultados

--- run_tracker.py
+++ run_tracker.py
@@ -43,38 +43,7 @@ def _track_sequences(sequence,net,ds):
     # sequence_bbox_list += bbox_list
     return init_frame, bbox_list
     print
-def _track_sequences_1(sequences,net,ds):
-    sequence_bbox_list = []
-    sequence_init_frame = int(sequences[['init_frame','final_frame']].min(1).iloc[0])
-    is_reversed = bool(sequences[['init_frame','final_frame']].apply(np.argmin,axis=1).iloc[0] )
-    for sequence in sequences.itertuples(index=False):
-        tracker = Tracker(name='transt', net=net, window_penalty=0.49, exemplar_size=128, instance_size=256)
-        x1,y1,x2,y2 = sequence[2]
-        cx, cy, w, h = get_axis_aligned_bbox(np.array([x1,y1,x2-x1,y2-y1]))
-        gt_bbox_ = [cx - w / 2, cy - h / 2, w, h]
 
-        init_info = {'init_bbox': gt_bbox_}
-        init_frame = sequence[3]
-        end_frame = sequence[4]
-
-        if is_reversed:
-            frame_range = list(reversed(range(end_frame,init_frame+1)))
-        else:
-            frame_range = list(range(init_frame,end_frame+1))
-
-        img_list = [cv2.cvtColor(cv2.imread(frame), cv2.COLOR_RGB2BGR) for frame in ds.iloc[frame_range]['img']]
-        tracker.initialize(img_list[0], init_info)
-        bbox_list = [sequence[2].tolist()]
-        for img in img_list[1:]:
-            outputs = tracker.track(img)
-            x1,y1,x2,y2 = outputs['target_bbox']
-            x2 = x1+x2
-            y2 = y1+y2
-            bbox_list.append([x1,y1,x2,y2])
-        if is_reversed:
-            bbox_list = reversed(bbox_list)
-        sequence_bbox_list += bbox_list
-    return sequence_init_frame, sequence_bbox_list
 def filter_annotation_label(ann_list,annotation_label,min_confidence):
     if len(ann_list) == 0:
         return []
@@ -124,14 +93,19 @@ def track_sequence(model_pth,ds,annotation_label,min_confidence):
         
         forward_sequences = ds.loc[ds.bbox.apply(lambda x: len(x)>0)]
         forward_sequences['final_frame'] = forward_sequences['init_frame'].shift(-1).fillna(total_frames).astype(int) -1 
+
+        sequences = forward_sequences
         
         backward_sequence = ds.loc[ds.bbox.apply(lambda x: len(x)>0)]
         backward_sequence['final_frame'] = backward_sequence['init_frame'].shift(1).fillna(-1).astype(int) + 1
         if backward_sequence.shape[0]>0:
             backward_sequence = backward_sequence.iloc[[0]]
-        sequences = pd.concat([backward_sequence,forward_sequences],ignore_index=True)
+            b_init_frame = int(backward_sequence.init_frame.values)
+            b_final_frame = int(backward_sequence.final_frame.values)
+            if b_init_frame != b_final_frame:
+                sequences = pd.concat([backward_sequence,sequences],ignore_index=True)
         # backward_init_frame, backward_bbox_list = _track_sequences(backward_sequences,net,ds)
-        if forward_sequences.shape[0]>0:
+        if sequences.shape[0] > 0:
             _out = sequences.apply(lambda x: _track_sequences(x,net,ds),axis=1)
             # _out = list(_out.apply(pd.DataFrame))
             # forward_init_frame, forward_bbox_list   = _track_sequences(forward_sequences,net,ds)
@@ -156,21 +130,23 @@ def track_dataset(ds, dataset,model_pth,output_path,annotation_label,min_confide
             out_file.append([ann_path,x1,y1,x2,y2])
             print
         out_file = pd.DataFrame(out_file, columns=['ann_path','x_min','y_min','x_max','y_max'])
+        out_file[['x_min','y_min','x_max','y_max']] = out_file[['x_min','y_min','x_max','y_max']].astype(float).round(2)
         out_file.to_csv(output_path,index=False)
         torch.cuda.empty_cache()
-def main(dataset_path, model_pth,annotation_label,min_confidence):
+def main(dataset_path, model_pth,annotation_label,output_path,min_confidence):
     image_project = Tset_Supervisely(dataset_path)
     image_project.load_anns(12)
-    
+    output_name = os.path.basename(dataset_path)+"__"+annotation_label
+    output_path = os.path.join(output_path,output_name)
+    os.makedirs(output_path,exist_ok=True)
     args = []
     for seq_label in image_project.samples.seq_label.unique():
         dataset = image_project.project.datasets.get(seq_label)
         ds = image_project.filter_by_seq(seq_label).samples
-        os.makedirs(f'output__{annotation_label}',exist_ok=True)
-        output_path = os.path.join(f'output__{annotation_label}',f"{seq_label}.csv")
-        if not os.path.isfile(output_path):
-            args.append([ds, dataset,model_pth,output_path,annotation_label,min_confidence])
-            # track_dataset(ds, dataset,model_pth,output_path,annotation_label,min_confidence)
+        _output_path = os.path.join(output_path,f"{seq_label}.csv")
+        if not os.path.isfile(_output_path):
+            args.append([ds, dataset,model_pth,_output_path,annotation_label,min_confidence])
+            # track_dataset(ds, dataset,model_pth,_output_path,annotation_label,min_confidence)
     pool = mp.Pool(2)
     pool.starmap(track_dataset,args)
     pool.close()
@@ -182,8 +158,10 @@ def main(dataset_path, model_pth,annotation_label,min_confidence):
 
 
 if __name__ == "__main__":
-    image_dataset_path = "/run/media/eduardo/SSD/Unifall/dataset/T-set/projects/Tset_images"
+    # image_dataset_path = "/run/media/eduardo/SSD/Unifall/dataset/T-set/projects/Tset_images"
+    image_dataset_path = "/run/media/eduardo/SSD/Unifall/dataset/LaSOT/lasot_person"
     model_path = "/run/media/eduardo/SSD/Unifall/modelos/TransT/transt.pth"
-    annotation_label = 'keypointrcnn_resnet50_fpn'
+    annotation_label = '0.01__ground_truth'
+    output_path = "output"
     min_confidence = 0.98
-    main(image_dataset_path, model_path,annotation_label,min_confidence)
+    main(image_dataset_path, model_path,annotation_label,output_path,min_confidence)
